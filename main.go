4cfc98327e054c762e648d81c3a9af75688e7e70
Rogerio Dos Santos Bispo <rogerio_pd@yahoo.com.br>
2023-04-29 21:19:29 -0300

    new version released

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 	println("Listening port 8080")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello world</h1>"))
+		w.Write([]byte("<h1>Hello Argo foda demais</h1>"))
 	})
 
 	http.ListenAndServe(":8080", nil)
